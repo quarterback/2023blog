1df59d60d92869b253fd58026e6abf8a058cf0cd
Ron Bronson <contact@ronbronson.com>
2022-12-02 18:13:32 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/quarterback/",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
